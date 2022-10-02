302948ef604e69cc03dec6616e92e23c82fdcb41
Undefined X <undefinedx@UndefinacStudio.attlocal.net>
2022-10-02 11:55:13 -0700

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import {useContext, useState} from "react"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <div className={"light"}>
+      <Component  {...pageProps} />
+      </div>
+  )
 }
 
 export default MyApp
